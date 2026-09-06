20e111028de6f929fbe7386abe381c1f0befccb8
Devedander <34205980+Devedander@users.noreply.github.com>
2026-09-06 15:17:31 -0700

    Position social icons before contact actions

--- app/new-concepts.tsx
+++ app/new-concepts.tsx
@@ -1443,13 +1443,13 @@ export default function NewConcepts(props: Props) {
             <button onClick={() => go('blog')}>Notes</button>
             <button onClick={() => go('gift')}>Gift cards</button>
           </nav>
-          <Action to="book">Schedule now</Action>
-          <a className="n-call" href="tel:+17073037707">Call <span>(707) 303-7707</span></a>
           <div className="n-social" aria-label="Social media links">
             <a href="https://www.facebook.com/search/top?q=santa%20rosa%20medical%20massage%2C%20inc.%20ca%2314021" target="_blank" rel="noreferrer" aria-label="Facebook"><img src="/social-facebook.svg" alt="" /></a>
             <a href="https://www.instagram.com/santarosamedicalmassage/" target="_blank" rel="noreferrer" aria-label="Instagram"><img src="/social-instagram.svg" alt="" /></a>
             <a href="https://www.yelp.com/biz/santa-rosa-medical-massage-santa-rosa-4" target="_blank" rel="noreferrer" aria-label="Yelp"><img src="/social-yelp.svg" alt="" /></a>
           </div>
+          <a className="n-call" href="tel:+17073037707">Call <span>(707) 303-7707</span></a>
+          <Action to="book">Schedule now</Action>
           {props.design === 'desk' && (
             <div className="desk-nav-bottom">
               <span>Here to help.</span>
