9540fec1c63d5a90ed584ecedb0d81b9ffeb7fad
Devedander <34205980+Devedander@users.noreply.github.com>
2026-09-06 15:07:51 -0700

    Add social links to every preview header

--- app/new-concepts.tsx
+++ app/new-concepts.tsx
@@ -1445,6 +1445,11 @@ export default function NewConcepts(props: Props) {
           </nav>
           <Action to="book">Schedule now</Action>
           <a className="n-call" href="tel:+17073037707">Call <span>(707) 303-7707</span></a>
+          <div className="n-social" aria-label="Social media links">
+            <a href="https://www.facebook.com/search/top?q=santa%20rosa%20medical%20massage%2C%20inc.%20ca%2314021" target="_blank" rel="noreferrer">Facebook</a>
+            <a href="https://www.instagram.com/santarosamedicalmassage/" target="_blank" rel="noreferrer">Instagram</a>
+            <a href="https://www.yelp.com/biz/santa-rosa-medical-massage-santa-rosa-4" target="_blank" rel="noreferrer">Yelp</a>
+          </div>
           {props.design === 'desk' && (
             <div className="desk-nav-bottom">
               <span>Here to help.</span>
